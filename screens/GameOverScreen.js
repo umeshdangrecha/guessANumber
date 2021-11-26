d29fbe11931ab9984910d1d39b66221a6851133b
Umesh Patidar <50135590+umeshdangrecha@users.noreply.github.com>
2021-11-26 12:19:48 +0530

    added network image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, Button, Image } from "react-native";
 import TitleText from "../components/TitleText";
 import BodyText from "../components/BodyText";
+import Colors from "../constants/color";
 
 const GameOverScreen = (props) => {
   return (
@@ -11,11 +12,19 @@ const GameOverScreen = (props) => {
         <Image
           style={styles.image}
           source={require("../assets/success.png")}
+          // source={{
+          //   uri: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+          // }} // for network image we have set height and width
           resizeMode="cover"
         />
       </View>
-      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
+      <BodyText>
+        Number of rounds:{" "}
+        <Text style={styles.highlight}>{props.roundsNumber}</Text>
+      </BodyText>
+      <BodyText>
+        Number was: <Text style={styles.highlight}>{props.userNumber}</Text>
+      </BodyText>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
@@ -32,6 +41,10 @@ const styles = StyleSheet.create({
     borderColor: "black",
     overflow: "hidden",
     marginVertical: 30,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold",
   },
 });
 
