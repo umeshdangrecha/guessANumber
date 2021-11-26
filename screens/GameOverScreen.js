d11c4e130bbb4d140c26ab6aa3c6ab07645601d4
Umesh Patidar <50135590+umeshdangrecha@users.noreply.github.com>
2021-11-26 12:02:49 +0530

    added local image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -7,7 +7,13 @@ const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>Game over</TitleText>
-      <Image source={require("../assets/success.png")} />
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/success.png")}
+          resizeMode="cover"
+        />
+      </View>
       <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title="NEW GAME" onPress={props.onRestart} />
@@ -17,6 +23,16 @@ const GameOverScreen = (props) => {
 
 const styles = StyleSheet.create({
   screen: { flex: 1, justifyContent: "center", alignItems: "center" },
+  image: { width: "100%", height: "100%" },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    width: 300,
+    height: 300,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 30,
+  },
 });
 
 export default GameOverScreen;
